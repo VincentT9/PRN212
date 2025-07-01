4a55ecc86b1a0311e5a71f03f72730444de12398
Nguyễn Công Tín <181190018+VincentT9@users.noreply.github.com>
2025-07-01 13:20:16 +0700

    Chapter8: LoginWindow

--- MyStoreWpfApp_EntityFrameWork/obj/Debug/net9.0-windows/MainWindow.g.i.cs
+++ MyStoreWpfApp_EntityFrameWork/obj/Debug/net9.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace MyStoreWpfApp_EntityFrameWork {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MyStoreWpfApp_EntityFrameWork;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MyStoreWpfApp_EntityFrameWork;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
